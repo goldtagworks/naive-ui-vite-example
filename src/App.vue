cd5193f2b5287fc2dd00d843dab2526c65d06c97
ian.lab(jaewon.lee)/melon <ian.lab@kakaocorp.com>
2024-10-25 15:32:42 +0900

    테스트

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <router-view />
+    <n-config-provider :theme="theme">
+        <router-view />
+    </n-config-provider>
 </template>
 
 <script setup lang="ts">
+import { darkTheme } from 'naive-ui';
+import { ref } from 'vue';
+
 defineOptions({
     name: 'App'
 });
+
+const theme = ref<any>(darkTheme);
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,68 @@
 <template>
-    <n-config-provider :theme="theme">
-        <n-card>
-            <n-space>
-                <n-button @click="theme = darkTheme">Dark</n-button>
-                <n-button @click="theme = null">Light</n-button>
-            </n-space>
-        </n-card>
-    </n-config-provider>
+    <n-card>
+        <n-space>
+            <n-button>Default</n-button>
+            <n-button type="tertiary"> Tertiary </n-button>
+            <n-button type="primary"> Primary </n-button>
+            <n-button type="info"> Info </n-button>
+            <n-button type="success"> Success </n-button>
+            <n-button type="warning"> Warning </n-button>
+            <n-button type="error"> Error </n-button>
+        </n-space>
+    </n-card>
+    <n-card>
+        <n-space>
+            <n-button strong secondary> Default </n-button>
+            <n-button strong secondary type="tertiary"> Tertiary </n-button>
+            <n-button strong secondary type="primary"> Primary </n-button>
+            <n-button strong secondary type="info"> Info </n-button>
+            <n-button strong secondary type="success"> Success </n-button>
+            <n-button strong secondary type="warning"> Warning </n-button>
+            <n-button strong secondary type="error"> Error </n-button>
+            <n-button strong secondary round> Default </n-button>
+            <n-button strong secondary round type="primary"> Primary </n-button>
+            <n-button strong secondary round type="info"> Info </n-button>
+            <n-button strong secondary round type="success"> Success </n-button>
+            <n-button strong secondary round type="warning"> Warning </n-button>
+            <n-button strong secondary round type="error"> Error </n-button>
+            <n-button strong secondary circle>
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+            <n-button strong secondary circle type="primary">
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+            <n-button strong secondary circle type="info">
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+            <n-button strong secondary circle type="success">
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+            <n-button strong secondary circle type="warning">
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+            <n-button strong secondary circle type="error">
+                <template #icon>
+                    <n-icon><CashIcon /></n-icon>
+                </template>
+            </n-button>
+        </n-space>
+    </n-card>
 </template>
 
 <script setup lang="ts">
-import { darkTheme } from 'naive-ui';
-import { ref } from 'vue';
+import { CashOutline as CashIcon } from '@vicons/ionicons5';
 
 defineOptions({
     name: 'App'
 });
-
-const theme = ref<any>(darkTheme);
 </script>
