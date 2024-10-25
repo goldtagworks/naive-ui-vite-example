e0a64dc9c6997107d955130f463d655d40ce3b6a
ian.lab(jaewon.lee)/melon <ian.lab@kakaocorp.com>
2024-10-25 12:21:12 +0900

    코드수정

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import naive from 'naive-ui';
 import App from './App.vue';
 import router from './router/index.js';
 import stores from './stores/index.js';
@@ -6,6 +7,7 @@ import stores from './stores/index.js';
 import './base.css';
 
 createApp(App)
+    .use(naive)
     .use(router)
     .use(stores)
     .mount('#app')
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,82 +1,21 @@
 <template>
-    <div>
-        <div>vue / vite / naive ui test</div>
-        <a href="https://vitejs.dev/" target="_blank">
-            <img src="../assets/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
-    <div class="flex-center">
-        <n-space vertical style="max-width: 360px">
-            <n-button>Naive UI SSR</n-button>
-            <n-cascader :options="cascaderOptions" />
-            <n-select :options="selectOptions" />
-            <n-input-number />
-        </n-space>
-    </div>
+    <n-config-provider :theme="theme">
+        <n-card>
+            <n-space>
+                <n-button @click="theme = darkTheme">Dark</n-button>
+                <n-button @click="theme = null">Light</n-button>
+            </n-space>
+        </n-card>
+    </n-config-provider>
 </template>
 
 <script setup lang="ts">
+import { darkTheme } from 'naive-ui';
 import { ref } from 'vue';
-import { NButton, NSpace, NInputNumber, NCascader, NSelect } from 'naive-ui';
 
 defineOptions({
-    name: 'IndexPage'
+    name: 'App'
 });
 
-const cascaderOptions = ref([
-    {
-        label: '0',
-        value: '0',
-        children: [
-            {
-                label: '0-0',
-                value: '0-0'
-            },
-            {
-                label: '0-1',
-                value: '0-1'
-            }
-        ]
-    }
-]);
-const selectOptions = ref([
-    {
-        label: '0',
-        value: '0'
-    },
-    {
-        label: '1',
-        value: '1'
-    }
-]);
+const theme = ref<any>(darkTheme);
 </script>
-
-<style>
-.flex-center {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-
-.logo.electron:hover {
-    filter: drop-shadow(0 0 2em #9feaf9);
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
